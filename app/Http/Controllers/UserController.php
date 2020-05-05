15f673bee377630aa17640463d734161ccab4b29
Ivinxi <64706012+Ivinxi@users.noreply.github.com>
2020-05-05 21:51:38 +0200

    Vista y cambio a User
    A ver si funcionan las sesiones uwu

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Usuario;
+use App\User;
 
 class UsuarioController extends Controller
 {
---
+++ app/User.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    protected $table = 'usuarios';
+
+    protected $primary_key = 'id_usuario';
+
+    public function factura()
+    {
+        return $this->hasMany('App\Factura', 'id_usuario', 'id_usuario');
+    }
+}
